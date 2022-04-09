f4bf61e96f3ed66e4f173ac67581376e65b64a59
Jared Leonguerrero <linuxpir8@gmail.com>
2022-04-09 00:13:38 -0500

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -20,7 +20,7 @@ import styles from './Footer.module.scss';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className="container">
+      < className="container">
         {appConfig?.socialLinks && (
           <div className={styles['social-links']}>
             <ul aria-label="Social media">
@@ -127,16 +127,11 @@ export default function Footer() {
           className={styles.nav}
           menuLocation={MENUS.FOOTER_LOCATION}
         />
-        <section>
-<div>
-<p class="member-text">A member of<br /><a href="https://agrilife.tamu.edu/"><span class="member-brand">Texas A&amp;M AgriLife</span></a></p>
-</div>
-<div id="member-list" class="wp-block-column">
-<p><a href="https://agrilifeextension.tamu.edu/">Texas A&amp;M AgriLife Extension Service</a> | <a href="https://agriliferesearch.tamu.edu/">Texas A&amp;M AgriLife Research</a> | <a href="https://tfsweb.tamu.edu/">Texas A&amp;M Forest Service</a> | <a href="https://tvmdl.tamu.edu/">Texas A&amp;M AgriLife Veterinary Medical Diagnostic Lab</a> | <a href="https://aglifesciences.tamu.edu/">College of Agriculture &amp; Life Sciences</a></p>
-</div/</section/>
-</div></div></section />
-</div></div>
-  <div class="wrap"><p><a rel="noreferrer noopener" href="http://agrilife.org/required-links/compact/" target="_blank">Compact with Texans</a> | <a rel="noreferrer noopener" href="http://agrilife.org/required-links/privacy/" target="_blank">Privacy and Security</a> | <a rel="noreferrer noopener" href="http://itaccessibility.tamu.edu/" target="_blank">Accessibility Policy</a> | <a rel="noreferrer noopener" href="http://publishingext.dir.texas.gov/portal/internal/resources/DocumentLibrary/State%20Website%20Linking%20and%20Privacy%20Policy.pdf" target="_blank">State Link Policy</a> | <a rel="noreferrer noopener" href="http://www.tsl.state.tx.us/trail" target="_blank">Statewide Search</a> | <a rel="noreferrer noopener" href="http://veterans.tamu.edu/" target="_blank">Veterans Benefits</a> | <a rel="noreferrer noopener" href="http://fcs.tamu.edu/families/military_families/" target="_blank">Military Families</a> | <a rel="noreferrer noopener" href="https://secure.ethicspoint.com/domain/en/report_custom.asp?clientid=19681" target="_blank">Risk, Fraud &amp; Misconduct Hotline</a> | <a rel="noreferrer noopener" href="http://www.texashomelandsecurity.com/" target="_blank">Texas Homeland Security</a> | <a rel="noreferrer noopener" href="http://veterans.portal.texas.gov/" target="_blank">Texas Veteran's Portal</a> | <a rel="noreferrer noopener" href="http://aghr.tamu.edu/education-civil-rights.htm" target="_blank">Equal Opportunity</a> | <a rel="noreferrer noopener" href="http://agrilife.org/required-links/orpi/" target="_blank">Open Records/Public Information</a></p></div/>
+        
+   <div>
+				<h1>Aboutpage</h1>
+				<Link to='/'>Go to Aboutpage</Link>
+		</div>
   
   <div className={styles.copyright}>
     &copy; {new Date().getFullYear()} Blueprint Media &#183; Powered By{' '}
