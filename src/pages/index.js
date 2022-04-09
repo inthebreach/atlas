1ac701c047c080e28bbe9db9abf358239bd3dfe5
Jared Leonguerrero <linuxpir8@gmail.com>
2022-04-08 21:10:48 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Page() {
   });
   const testimonials = useQuery().testimonials();
   const mainBanner = {
-    sourceUrl: '/static/banner.jpeg',
+    sourceUrl: '/static/banner.png',
     mediaDetails: { width: 1200, height: 600 },
     altText: 'Portfolio Banner',
   };
